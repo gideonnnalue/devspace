43610a88d08ab8eb61877ce146f5068113978552
Gideon Nnalue <emekannalue@gmail.com>
2019-06-06 08:05:57 -0400

    Initial express server with route

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://devconnector:devconnector@devconnector-rojvm.mongodb.net/test?retryWrites=true&w=majority',
+    mongoURILocal: 'mongodb://gideon:gideon5053@localhost:27017/devconnectors'
+}
