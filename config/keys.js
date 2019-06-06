d1ba68aec19f7b9107544faf58f34af9b1579f02
Gideon Nnalue <emekannalue@gmail.com>
2019-06-06 12:35:42 -0400

    Finished Authentication and Validation of user

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: 'mongodb+srv://devconnector:devconnector@devconnector-rojvm.mongodb.net/test?retryWrites=true&w=majority',
-    mongoURILocal: 'mongodb://gideon:gideon5053@localhost:27017/devconnectors'
+    mongoURILocal: 'mongodb://gideon:gideon5053@localhost:27017/devconnectors',
+    secretOrKey: 'secret'
 }
