db77bd5c5cb6847d06379e32dcde0b3e130077a3
Gideon Nnalue <emekannalue@gmail.com>
2019-06-09 12:42:31 -0400

    Registration and error reducer

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import setAuthToken from '../utils/setAuthToken';
+
+import { GET_ERRORS } from "./types";
+
+// Register User
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post("/api/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+        // Save to localStorage
+        const { token } = res.data;
+        // Set token to localStorage
+        localStorage.setItem('jwtToken', token);
+        // set token to Auth header
+        setAuthToken(token);
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
